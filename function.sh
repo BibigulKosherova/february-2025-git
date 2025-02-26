2dfb2ccf55cb3c3704f4a32f0e5619361748079c
BibigulKosherova <bibigulkosherova@gmail.com>
2025-02-26 02:55:21 +0000

    new scripts

---
+++ function.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+function folder() {
+	mkdir folder1
+	mkdir folder2
+	mkdir folder3
+}
+
+folder
+
+function file() {
+	touch world1
+	touch world2
+	touch world3
+}
+
+
+function user() {
+	sudo useradd ann
+	sudo useradd brad
+}
+
+file
+user
---
+++ text.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo Hello my name is Bibigul
+echo Bibigul is currently studying Linux
+echo Bibigul loves DevOps
+echo $((26*30*12))
